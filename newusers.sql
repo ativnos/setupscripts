1327d995fe6d3f5919135d190741c1cf8cb3609d
Evan Roman <evanroman1@gmail.com>
2014-08-01 22:14:57 -0700

    Update newusers.sql

--- newusers.sql
+++ newusers.sql
@@ -1,4 +1,6 @@
 CREATE role admin LOGIN SUPERUSER;
 CREATE role apps LOGIN; 
+CREATE role slony LOGIN;
 -- ALTER ROLE admin ENCRYPTED PASSWORD 'password'
--- ALTER ROLE apps ENCRYPTED PASSWORD 'password'
+-- ALTER ROLE apps PASSWORD 'password'
+-- ALTER ROLE slony ENCRYPTED PASSWORD 'password'
